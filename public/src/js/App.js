e7d7906384a2784807e78fad015c0ed5f40c662b
Bogod <Daniel.Bogod@historicengland.org.uk>
2019-08-15 01:09:39 +0100

    Remove eslint-disable

--- public/src/js/App.js
+++ public/src/js/App.js
@@ -1,10 +1,9 @@
-/*eslint-disable*/
 import React, { Component } from 'react';
 
 import Form from './components/Form';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       kb: false
